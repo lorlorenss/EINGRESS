f4b623c46466c863f3a0a469df153d3b6fe782e7
Hime Hime <162676818+mimimimimixo@users.noreply.github.com>
2024-03-19 12:39:32 +0800

    online database edit

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -16,8 +16,9 @@ import { AdminLoginModule } from './admin-login/admin-login.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      url: process.env.POSTGRES_URL,
       host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
+      // port: parseInt(<string>process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
