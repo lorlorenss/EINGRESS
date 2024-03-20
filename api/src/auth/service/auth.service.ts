3ae684e768fc10fd7018344d71ee62ea61206f20
Hime Hime <162676818+mimimimimixo@users.noreply.github.com>
2024-03-20 15:20:21 +0800

    Update auth.service.ts
    update for password error

--- api/src/auth/service/auth.service.ts
+++ api/src/auth/service/auth.service.ts
@@ -2,14 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable, from, of } from 'rxjs';
 import { User } from 'src/admin-login/models/user.interface';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const bcrypt = require('bcrypt');
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  comparePasswords(password: string, password1: string) {
-    throw new Error('Method not implemented.');
-  }
+
   constructor(private readonly jwtService: JwtService) {}
 
   generateJWT(user: User): Observable<string> {
@@ -17,23 +14,11 @@ export class AuthService {
   }
 
   hashPassword(password: string): Observable<string> {
-    return from(<string>bcrypt.hash(password, 12));
+    return from(bcrypt.hash(password, 12));
   }
 
-  comparePassword(
-    newPassword: string,
-    passwordHash: string,
-  ): Observable<any | boolean> {
-    return of<any | boolean>(bcrypt.compare(newPassword, passwordHash));
+  comparePassword(password: string, hashedPassword: string): Observable<boolean> {
+    return from(bcrypt.compare(password, hashedPassword));
   }
-
-  // comparePassword(
-  //   newPassword: string,
-  //   passwordHash: string,
-  // ): Observable<any | boolean> {
-  //   const result = bcrypt.compareSync(newPassword, passwordHash);
-  //   return of<any | boolean>(result);
-  // }
-  
   
 }
