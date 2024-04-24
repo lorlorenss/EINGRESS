3771b66bdb8d00903833c16b50ebd8b3736cf347
Hime Hime <162676818+mimimimimixo@users.noreply.github.com>
2024-04-24 13:37:20 +0800

    user can now upload the photo along with other details
    please work on the frontend part, i tried XD

--- api/src/employee-list/controllers/employee.controller.ts
+++ api/src/employee-list/controllers/employee.controller.ts
@@ -1,15 +1,44 @@
-import { Body, Controller, Get, Param, Post , Delete, Put, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post , Delete, Put, NotFoundException, BadRequestException, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { EmployeeService } from '../services/employee.service';
 import { Employee } from '../models/employee.interface';
-import { Observable, catchError, map } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
+import {FileInterceptor} from '@nestjs/platform-express'
+import { diskStorage } from 'multer';
+import {v4 as uuid4} from 'uuid';
+import * as path from 'path';
+
+export const storage = {
+  storage: diskStorage({
+    destination: './uploads/profileimages',
+    filename: (req,file, cb) => {
+      const filename: string = path.parse(file.originalname).name.replace(/\s/g,'') + uuid4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`)
+    }
+  })
+}
 
 @Controller('employee')
 export class EmployeeController {
     constructor(private userService: EmployeeService) {}
 
+    // @Post()
+    // create(@Body() employee: Employee): Observable<Employee | Object> {
+    //     return this.userService.create(employee);
+    // }
     @Post()
-    create(@Body() employee: Employee): Observable<Employee | Object> {
-        return this.userService.create(employee);
+    @UseInterceptors(FileInterceptor('file', storage))
+    create(@Body() employee: Employee, @UploadedFile() file): Observable<Employee | Object> {
+      if (!file) {
+        throw new BadRequestException('Image file is required');
+      }
+  
+      // Assign the file path to the employee object
+      employee.profileImage = file.path;
+  
+      // Call the service method to create the employee
+      return this.userService.create(employee);
     }
 
     @Get(':id') // Route for findOne
@@ -46,6 +75,14 @@ export class EmployeeController {
       }
     
       return this.userService.logEmployeeAccess(employeeId, accessType, roleAtAccess).toPromise();
+    }
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file',storage))
+    uploadFile(@UploadedFile()file): Observable<Object> {
+      console.log(file);
+      return of({imagePath: file.path});
+
     }
     
 }
