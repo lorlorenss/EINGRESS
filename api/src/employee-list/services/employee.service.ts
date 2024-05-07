638b066699d8adc06ccd37a260776a5ba4fe7a6b
Hime Hime <162676818+mimimimimixo@users.noreply.github.com>
2024-05-07 13:23:25 +0800

    fixed error for lastlogdate
    fixed error for lastlogdate

--- api/src/employee-list/services/employee.service.ts
+++ api/src/employee-list/services/employee.service.ts
@@ -65,20 +65,21 @@ create(employee: Employee): Observable<Employee> {
             // const dateOnly = this.getOnlyDate(new Date().toISOString());
             // employee.lastlogdate = dateOnly;
 
-               //last logdate has time and date
-               const currentDate = new Date();
-               const options: Intl.DateTimeFormatOptions = {
-                 year: 'numeric', 
-                 month: '2-digit', 
-                 day: '2-digit',
-                 hour: '2-digit', 
-                 minute: '2-digit', 
-                 second: '2-digit',
-                 hour12: false, // Use 24-hour format
-                 timeZone: 'Asia/Manila' // Set the time zone to Philippine time
-               };
-               const dateAndTimeInPhilippineTime = currentDate.toLocaleString('en-PH', options);
-               employee.lastlogdate = dateAndTimeInPhilippineTime;
+             //last logdate has time and date
+             const currentDate = new Date();
+const options: Intl.DateTimeFormatOptions = {
+  year: 'numeric', 
+  month: '2-digit', 
+  day: '2-digit',
+  hour: '2-digit', 
+  minute: '2-digit', 
+  second: '2-digit',
+  hour12: false, // Use 24-hour format
+  timeZone: 'Asia/Manila' // Set the time zone to Philippine time
+};
+const dateAndTimeInPhilippineTime = currentDate.toLocaleString('en-PH', options);
+employee.lastlogdate = dateAndTimeInPhilippineTime;
+
       
             // Save the updated employee
             return from(this.userRepository.save(employee)).pipe(
