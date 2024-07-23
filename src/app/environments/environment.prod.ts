e590b58d3389f73a5255e3fdc9132c311e6d279b
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-07-23 16:21:27 +0800

    changes port to 8080 and baseUrl to localhost

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production : true,
-    //baseURL : 'http://192.168.42.68:4200/'
-    // baseURL : 'http://192.168.42.64:4200/' 
-    baseURL: 'http://nestjs:3000/'  // URL for Docker network
+    baseURL: 'http://localhost:3000/' 
 }
