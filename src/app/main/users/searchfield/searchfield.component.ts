f0a31d672cf46b1e6ea3b68956acec233c58ac71
lorlorenss <l.anasco.513898@umindanao.edu.ph>
2024-08-07 09:40:30 +0800

    clearing searchfield when employeeservice reload is triggered from exiting employee details

--- src/app/main/users/searchfield/searchfield.component.ts
+++ src/app/main/users/searchfield/searchfield.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { EmployeeService } from 'src/app/services/employee.service';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-searchfield',
   templateUrl: './searchfield.component.html',
@@ -9,20 +9,26 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class SearchfieldComponent {
 
   @ViewChild('searchInput') searchInput!: ElementRef;
-
   searchEmployee: string = ''
   isFocused: boolean = false;
+  private reloadSubscription: Subscription = new Subscription();
 
-  constructor(private searchUserService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.reloadSubscription = this.employeeService.reload$.subscribe(() => {
+      this.searchInput.nativeElement.value = "";
+    });
+  }
 
   onSearchUserInputChanged(){
     this.searchEmployee = this.searchInput.nativeElement.value;
-    this.searchUserService.triggerSearchUser(this.searchEmployee);
+    this.employeeService.triggerSearchUser(this.searchEmployee);
   }
 
   onInputBlur(){
     if(!this.searchEmployee.trim()){
-      this.searchUserService.triggerSearchUser('');
+      this.employeeService.triggerSearchUser('');
     }
   }
 
